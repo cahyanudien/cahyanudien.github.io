f4ae02d3ed0f89be9bb70f4b997077309cada1da
Cahyanudien Aziz Saputra <47025238+cas8398@users.noreply.github.com>
2026-03-07 14:25:22 +0700

    add wikidata

--- src/data/schema.js
+++ src/data/schema.js
@@ -2,8 +2,23 @@ export const schemaData = [
   {
     "@context": "https://schema.org",
     "@type": "Person",
+    "@id": "https://www.wikidata.org/wiki/Q138566666",
     name: "Cahyanudien Aziz Saputra",
     alternateName: "Cahyanudien",
+    identifier: [
+      {
+        "@type": "PropertyValue",
+        propertyID: "Wikidata",
+        value: "Q138566666",
+        url: "https://www.wikidata.org/wiki/Q138566666",
+      },
+      {
+        "@type": "PropertyValue",
+        propertyID: "ORCID",
+        value: "0009-0007-5983-9221",
+        url: "https://orcid.org/0009-0007-5983-9221",
+      },
+    ],
     url: "https://www.cahyanudien.site",
     description:
       "Full-stack developer, founder of FlagoDNA, author of Stillness That Walks and Hening yang Berjalan. Building meaningful digital ecosystems since 2020. 60K+ users across 15+ apps. Philosophy: Mlampah Ing Tresno.",
@@ -20,17 +35,32 @@ export const schemaData = [
       "https://github.com/cas8398",
       "https://github.com/cahyanudien",
       "https://linkedin.com/in/cahyanudien",
+      "https://www.wikidata.org/wiki/Q138566666",
+      "https://orcid.org/0009-0007-5983-9221",
+    ],
+    subjectOf: [
+      {
+        "@type": "Book",
+        name: "Stillness That Walks",
+      },
+      {
+        "@type": "Book",
+        name: "Hening yang Berjalan",
+      },
     ],
   },
   {
     "@context": "https://schema.org",
     "@type": "Book",
+    "@id": "https://cahyanudien.github.io/stillness-that-walks/#book",
     name: "Stillness That Walks",
     alternateName: "Stillness That Walks: message within message",
     author: {
       "@type": "Person",
+      "@id": "https://www.wikidata.org/wiki/Q138566666",
       name: "Cahyanudien Aziz Saputra",
       url: "https://www.cahyanudien.site",
+      sameAs: "https://www.wikidata.org/wiki/Q138566666",
     },
     url: "https://cahyanudien.github.io/stillness-that-walks/",
     genre: "Philosophical Poetry",
@@ -56,12 +86,15 @@ export const schemaData = [
   {
     "@context": "https://schema.org",
     "@type": "Book",
+    "@id": "https://cahyanudien.github.io/hening-yang-berjalan/#book",
     name: "Hening yang Berjalan",
     alternateName: "Hening yang Berjalan: pesan dalam pesan",
     author: {
       "@type": "Person",
+      "@id": "https://www.wikidata.org/wiki/Q138566666",
       name: "Cahyanudien Aziz Saputra",
       url: "https://www.cahyanudien.site",
+      sameAs: "https://www.wikidata.org/wiki/Q138566666",
     },
     url: "https://cahyanudien.github.io/hening-yang-berjalan/",
     genre: "Philosophical Poetry",
@@ -85,7 +118,11 @@ export const schemaData = [
     name: "FlagoDNA",
     url: "https://flagodna.com",
     foundingDate: "2020",
-    founder: { "@type": "Person", name: "Cahyanudien Aziz Saputra" },
+    founder: {
+      "@type": "Person",
+      "@id": "https://www.wikidata.org/wiki/Q138566666",
+      name: "Cahyanudien Aziz Saputra",
+    },
     description:
       "Privacy-first digital ecosystems. 60K+ users across 15+ apps.",
   },
