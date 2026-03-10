54076b2ce1167760aa95f0e03067f689d87387dc
Cahyanudien Aziz Saputra <47025238+cas8398@users.noreply.github.com>
2026-03-11 00:54:34 +0700

    fix build for ache

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,11 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         stillness: resolve(__dirname, "stillness-that-walks/index.html"),
         hening: resolve(__dirname, "hening-yang-berjalan/index.html"),
+        ache_en: resolve(
+          __dirname,
+          "ache-what-kind-of-life-is-this/index.html"
+        ),
+        ache_id: resolve(__dirname, "ache-hidup-seperti-apa-ini/index.html"),
       },
     },
   },
